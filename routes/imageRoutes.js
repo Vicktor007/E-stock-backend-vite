be72945b642cadc12bd8520238847249d016a5de
Vicktor007 <vicktord007@gmail.com>
2024-01-25 12:55:41 +0100

    image upload edit

--- routes/imageRoutes.js
+++ routes/imageRoutes.js
@@ -1,4 +1,6 @@
-const cloudinary = require('cloudinary');
+const Product = require('../models/productModel');
+
+const cloudinary = require('cloudinary').v2;
 const router = require('express').Router();
 require('dotenv').config();
 
@@ -8,15 +10,30 @@ cloudinary.config({
   api_secret: process.env.API_SECRET
 })
 
+
 router.delete('/:public_id', async(req, res)=> {
   const {public_id} = req.params;
   try {
+      // Delete image from Cloudinary
       await cloudinary.uploader.destroy(public_id);
+
+      // Find product that contains the image
+      const product = await Product.findOne({ 'images.public_id': public_id });
+
+      if (product) {
+        // Remove the image from the product's images array
+        product.images = product.images.filter(image => image.public_id !== public_id);
+
+        // Save the product
+        await product.save();
+      }
+
       res.status(200).send();
   } catch (e) {
       res.status(400).send(e.message)
   }
 })
+
 
 
 module.exports = router;
